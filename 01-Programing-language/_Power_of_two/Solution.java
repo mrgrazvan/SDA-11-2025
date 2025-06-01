de262b5470de8e539ffae8adcb196cf8d7fb7a30
mrgrazvan <razvan.margine78@e-uvt.ro>
2025-06-01 13:38:51 +0000

    add orig power of two

---
+++ 01-Programing-language/_Power_of_two/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & n - 1) == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
